f1c935619387f798d488919925943bc75148235b
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-11-02 14:11:10 +0100

    DONE

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,7 +5,10 @@ function Form(props) {
     <form className="form">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
+      RENDERING THE CONFIRM PASSWORD IF THE USER IS NOT REGISTERED
+      {props.isRegistered === false && (
+        <input type="password" placeholder="Confirm Password" />
+      )}
       <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
     </form>
   );
